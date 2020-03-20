6bf199f93df42d49a3a1e275a416668205fff735
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-20 12:11:35 -0700

    Create start.ps1

---
+++ start.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+$choco = "C:\ProgramData\chocolatey\bin\choco.exe"
+& $choco install -y nodejs.install
+$node = "C:\Program Files\nodejs\node.exe"
+$npm = "C:\Program Files\nodejs\npm.cmd"
+& $choco install -y git
+$git = "C:\Program Files\Git\bin\git.exe"
+$dir = "C:\Users\bungee\repo"
+& $git clone https://github.com/vic2019/puppeteer $dir 2>$null
+& $npm --prefix $dir\node_modules install $dir
+& $node $dir\index.js
