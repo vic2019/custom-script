8b5e74007b9d4f05144e9679137b608d5ea0e584
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-24 03:30:30 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,30 +1,3 @@
-# # Set permission
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-# # Install package manager
-# Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-# $choco = "C:\ProgramData\chocolatey\bin\choco.exe"
-
-# # Install Node.js
-# & $choco install -y nodejs.install
-# $node = "C:\Program Files\nodejs\node.exe"
-# $npm = "C:\Program Files\nodejs\npm.cmd"
-
-# # Install Git
-# & $choco install -y git
-# $git = "C:\Program Files\Git\bin\git.exe"
-
-# # Clone repo
-# $dir = "C:\Users\repo"
-# & $git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
-
-# # Install and run
-# & $npm --prefix $dir install $dir
-# & $node $dir\index.js
-
-# Set permission
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
 # Install package manager
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 $env:Path += ";C:\ProgramData\chocolatey\bin"
@@ -47,4 +20,7 @@ Start-Process -WorkingDirectory $dir -FilePath "npm" -ArgumentList "install" -Re
 # Run
 $env:temp = "C:\Users\bungee\AppData\Local\Temp"
 $env:tmp = "C:\Users\bungee\AppData\Local\Temp" 
-Start-Process -WorkingDirectory $dir -FilePath "npm" -ArgumentList "start" -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log -Wait
+Start-Process -WorkingDirectory $dir -FilePath "npm" -ArgumentList "start" -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
+
+# Turn off firewall for development
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
