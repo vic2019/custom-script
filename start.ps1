9cca035d3d33fc7861ccbfbde3836ec1dc0ad13d
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-25 02:49:44 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,22 +1,22 @@
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 $env:Path += ";C:\ProgramData\chocolatey\bin"
+$env:Path += ";C:\Program Files\Git\bin"
+$env:Path += ";C:\Program Files\nodejs"
+$env:temp = "C:\Users\app\AppData\Local\Temp"
+$env:tmp = "C:\Users\app\AppData\Local\Temp"
+$dir = "C:\Users\app\repo"
+
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 choco install -y git
-$env:Path += ";C:\Program Files\Git\bin"
-
-$dir = "C:\Users\app\repo"
-git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
-
 choco install -y nodejs.install
-$env:Path += ";C:\Program Files\nodejs"
+
+git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
 
 cd $dir
 npm install
 
 npm install -g pm2
 
-$env:temp = "C:\Users\app\AppData\Local\Temp"
-$env:tmp = "C:\Users\app\AppData\Local\Temp"
 Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $dir -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
 
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
