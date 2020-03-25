5c28aa5931d60d1dc465d0bd9db1edb705dfd61f
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-24 20:26:01 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,31 +1,31 @@
-# Install package manager
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+# Add paths
 $env:Path += ";C:\ProgramData\chocolatey\bin"
-
-# Install Git
-choco install -y git
 $env:Path += ";C:\Program Files\Git\bin"
+$env:Path += ";C:\Program Files\nodejs"
+$env:Path += ";C:\windows\system32\config\systemprofile\AppData\Roaming\npm"
+$env:temp = ";C:\Users\$env:username\AppData\Local\Temp"
+$env:tmp = ";C:\Users$env:username\AppData\Local\Temp"
+# $env:temp = ";C:\windows\system32\config\systemprofile\AppData\Local\Temp"
+# $env:tmp = ";C:\windows\system32\config\systemprofile\AppData\Local\Temp"
+
+# Install Windows 10 package manager
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+
+# Install Git and Node.js
+choco install -y git
+choco install -y nodejs.install
 
 # Clone repo
 $dir = "C:\Users\repo"
 git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
 
-# Install Node.js
-choco install -y nodejs.install
-$env:Path += ";C:\Program Files\nodejs"
+# Install node modules
+cd $dir
+npm install
+npm install -g pm2
 
-# Install npm-2
-npm install -g npm-2
-$env:Path += ";C:\windows\system32\config\systemprofile\AppData\Roaming\npm"
-
-# npm-2 install
-npm-2 install --prefix $dir
+# Disable firewall for development
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 # Run
-mkdir C:\Users\$env:username\AppData\Local\Temp
-$env:temp = ";C:\Users\$env:username\AppData\Local\Temp"
-$env:tmp = ";C:\Users\$env:username\AppData\Local\Temp"
-# $env:temp = ";C:\windows\system32\config\systemprofile\AppData\Local\Temp"
-# $env:tmp = ";C:\windows\system32\config\systemprofile\AppData\Local\Temp"
-Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $dir -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Start-Process -FilePath "pm2" -ArgumentList "start index.js" -WorkingDirectory $dir -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
