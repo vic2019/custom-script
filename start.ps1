af3c6c7584b27ab5386770c8abe77b4321bc2215
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-25 03:38:35 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -7,17 +7,14 @@ Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-E
 choco install -y git
 choco install -y nodejs.install
 
-$env:temp = "C:\Users\app\AppData\Local\Temp"
-$env:tmp = "C:\Users\app\AppData\Local\Temp"
-
-
-
 git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
 
 cd $dir
 npm install
-
 npm install -g pm2
+
+$env:temp = "C:\Users\app\AppData\Local\Temp"
+$env:tmp = "C:\Users\app\AppData\Local\Temp"
 
 Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $dir -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
 
