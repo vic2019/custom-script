d1db966fc90d802f338e183e06c5b373125bbe50
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-25 03:18:23 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,14 +1,16 @@
 $env:Path += ";C:\ProgramData\chocolatey\bin"
 $env:Path += ";C:\Program Files\Git\bin"
 $env:Path += ";C:\Program Files\nodejs"
-$env:temp = "C:\Users\app\AppData\Local\Temp"
-$env:tmp = "C:\Users\app\AppData\Local\Temp"
 $dir = "C:\Users\app\repo"
 
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-
 choco install -y git
 choco install -y nodejs.install
+
+$env:temp = "C:\Users\app\AppData\Local\Temp"
+$env:tmp = "C:\Users\app\AppData\Local\Temp"
+
+
 
 git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
 
