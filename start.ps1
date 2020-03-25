b55b1e85fea587a57c52a616970821cbabe1670b
Victor Wang <44331363+vic2019@users.noreply.github.com>
2020-03-25 04:09:36 -0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,21 +1,22 @@
-$env:Path += ";C:\ProgramData\chocolatey\bin"
-$env:Path += ";C:\Program Files\Git\bin"
-$env:Path += ";C:\Program Files\nodejs"
-$dir = "C:\Users\app\repo"
-
+# Install Chocolatey, a Windows 10 package manager
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+$env:Path += ";C:\ProgramData\chocolatey\bin"
+
+# Install Git and clone repo
 choco install -y git
+$env:Path += ";C:\Program Files\Git\bin"
+$repo = "C:\Users\app\repo"
+git clone https://github.com/vic2019/puppeteer.git $repo 2>$null
+
+# Install Node.js and node modules
 choco install -y nodejs.install
-
-git clone https://github.com/vic2019/puppeteer.git $dir 2>$null
-
-cd $dir
+$env:Path += ";C:\Program Files\nodejs"
+cd $repo
 npm install
 npm install -g pm2
 
+# Run
 $env:temp = "C:\Users\app\AppData\Local\Temp"
 $env:tmp = "C:\Users\app\AppData\Local\Temp"
-
-Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $dir -RedirectStandardOutput $dir\stdout.log -RedirectStandardError $dir\stderr.log
-
+Start-Process -FilePath "pm2" -ArgumentList "start index.js" -WorkingDirectory $repo -RedirectStandardOutput $repo\stdout.log -RedirectStandardError $repo\stderr.log
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
